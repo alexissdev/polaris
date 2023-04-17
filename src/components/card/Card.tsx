64403c772783e911b977db343e6f1fb20545da33
alexissdev <notcacha1998@gmail.com>
2023-04-17 20:41:43 -0300

    refactor(card): added the custom color of button

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -22,7 +22,7 @@ export default function Card({ card }: { card: CardRepository }) {
           <span className="text-3xl font-bold text-white">${card.price}</span>
           <a
             href="#"
-            className="text-white bg-blue-600 hover:bg-blue-700  focus:ring-4 focus:outline-none focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            className="text-white bg-secondary hover:bg-[#4B2160] focus:ring-4 focus:outline-none focus:ring-secondary font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           >
             Comprar
           </a>
