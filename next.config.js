ead1433dd41aae0bbd51dfffe287c082a7a9ffb8
alexissdev <notcacha1998@gmail.com>
2023-04-19 15:09:33 -0300

    chore: added extra configs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  env: {
+    API_URL: process.env.API_URL,
+    COMPANY_NAME: process.env.COMPANY_NAME,
+    COMPANY_DESCRIPTION: process.env.COMPANY_DESCRIPTION,
+  },
 };
 
 module.exports = nextConfig;
