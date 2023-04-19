dbfef74e34f79ec8bdb9ff975b70cd13574d6c30
alexissdev <notcacha1998@gmail.com>
2023-04-19 11:59:09 -0300

    feat(cart): added the Cart and CartDetails component

---
+++ src/components/cart/Cart.tsx
@@ -0,0 +1,38 @@
+import CartProductRepository from "@/libs/product/cart.product.repository";
+
+import BubbleAlert from "@/components/alert/BubbleAlert";
+import CartDetails from "@/components/cart/CartDetails";
+
+export default function Cart({
+  cart,
+  viewCart,
+  toggleCart,
+}: {
+  cart: CartProductRepository[];
+  viewCart: boolean;
+  toggleCart: () => void;
+}) {
+  const amount = cart.reduce((acc, product) => acc + product.amount, 0);
+
+  return (
+    <div>
+      <span className="relative left-3 top-5">
+        {amount > 0 && <BubbleAlert value={amount} />}
+      </span>
+
+      <button
+        onClick={toggleCart}
+        className="border-none cursor-pointer appearance-none"
+      >
+        {
+          <img
+            src="/images/cart.svg"
+            alt="cart"
+            className="border-0 w-16 h-16 align-middle"
+          />
+        }
+      </button>
+      {viewCart && <CartDetails cart={cart} />}
+    </div>
+  );
+}
---
+++ src/components/cart/CartDetails.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import CartProductRepository from "@/libs/product/cart.product.repository";
+
+export default function CartDetails({
+  cart,
+}: {
+  cart: CartProductRepository[];
+}) {
+  return (
+    <div className="absolute mt-8 shadow-sm rounded-md w-80 right-0">
+      <ul className="m-o p-0">
+        {cart.map((product) => (
+          <li
+            key={product.name}
+            className="flex justify-between items-center py-6 px-5 rounded-sm bg-black list-none text-white"
+          >
+            <Image
+              src={product.imageUrl}
+              alt={product.name}
+              width={50}
+              height={32}
+            />
+            <span>{product.name}</span>
+            {""}
+            <span>{product.amount}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
