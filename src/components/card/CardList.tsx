236616b9df3a58cd8d95127269c6c18ef78056ad
alexissdev <notcacha1998@gmail.com>
2023-04-19 20:58:12 -0300

    refactor(card): change the gap and px of card list

--- src/components/card/CardList.tsx
+++ src/components/card/CardList.tsx
@@ -16,7 +16,7 @@ export default function CardList({
       <h1 className="p-8 text-white/80 font-medium text-3xl sm:text-4xl md:text-5xl">
         {title}
       </h1>
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-8 px-8">
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 px-3">
         {products.map((product) => (
           <ProductCard
             addToCart={addToCart}
